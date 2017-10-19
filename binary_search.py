a13af5f37d7e22099d5b174586a63a202fc87a86
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2017-10-19 22:40:44 +0530

    program for binary search

--- binary_search.py
+++ binary_search.py
@@ -1,15 +1,16 @@
-def binarySearch(alist, item):
+def binarySearch(list, item):
 	    first = 0
-	    last = len(alist)-1
+	    last = len(list)-1
 	    found = False
 	
 	    while first<=last and not found:
 	        midpoint = (first + last)//2
-	        if alist[midpoint] == item:
+	        if list[midpoint] == item:
 	            found = True
 	        else:
-	            if item < alist[midpoint]:
+	            if item < list[midpoint]:
 	                last = midpoint-1
             else:
 	                first = midpoint+1	
+
 	    return found
