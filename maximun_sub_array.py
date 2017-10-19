c161bc95f85421b18f7f0f1c9eb0a815f73eafa3
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2017-10-19 22:46:34 +0530

    program for maximum sub array

---
+++ maximun_sub_array.py
@@ -0,0 +1,25 @@
+def maxSubArray(ls):
+    if len(ls) == 0:
+       raise Exception("Array empty") 
+      
+    runSum = maxSum = ls[0]
+    i = 0
+    start = finish = 0
+
+    for j in range(1, len(ls)):
+    	if ls[j] > (runSum + ls[j]):
+            runSum = ls[j]
+            i = j
+        else:
+            runSum += ls[j]
+
+        if runSum > maxSum:
+            maxSum = runSum
+            start = i
+            finish = j
+
+    print "maxSum =>", maxSum
+    print "start =>", start, "; finish =>", finish
+
+maxSubArray([-2, 11, -4, 13, -5, 2])
+maxSubArray([-15, 29, -36, 3, -22, 11, 19, -5])
