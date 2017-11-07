ed98dc6394aea5c58e29ac51c34251ac6c89aa9d
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2017-11-07 19:50:44 +0530

    program to display calender

--- calender.py
+++ calender.py
@@ -1,14 +1,10 @@
-#Python program to display calendar of given month of the year
-
-# import module
 import calendar
 
 yy = 2014
 mm = 11
 
-# To ask month and year from the user
-# yy = int(input("Enter year: "))
-# mm = int(input("Enter month: "))
 
-# display the calendar
+ yy = int(input("Enter year: "))
+ mm = int(input("Enter month: "))
+
 print(calendar.month(yy, mm))
