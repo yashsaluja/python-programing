18ab7cd6b94395e6b8e485a1e16108319f735634
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2017-11-09 22:21:17 +0530

    program to add two matrix

--- Add_matrix.py
+++ Add_matrix.py
@@ -10,9 +10,9 @@ result = [[0,0,0],
      [0,0,0],
      [0,0,0]]
 
-# iterate through rows
+
+
 for i in range(len(X)):
-# iterate through columns
 for j in range(len(X[0])):
    result[i][j] = X[i][j] + Y[i][j]
 
