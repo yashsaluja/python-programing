7765629ef775abc18ead4ad0ac1aefbd9fe9bc28
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2017-11-24 23:13:18 +0530

    program uses appwnd insert and count

--- append.py
+++ append.py
@@ -1,18 +1,17 @@
 # New int array.
 a = array("i")
 
-# Append three integers.
 a.append(100)
 a.append(200)
 a.append(300)
 
-# Insert an integer at index 1.
+
 a.insert(1, 900)
 
-# Remove this element.
+
+
 a.remove(200)
 
-# Count elements with this value.
 a.count(900)
 
 # Print.
