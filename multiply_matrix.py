799240483c3e746cf508af7ca43d7c840e733d56
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2017-11-27 23:03:29 +0530

    multiplication of two matrix

--- multiply_matrix.py
+++ multiply_matrix.py
@@ -10,11 +10,11 @@ result = [[0,0,0,0],
          [0,0,0,0],
          [0,0,0,0]]
 
-# iterate through rows of X
+
 for i in range(len(X)):
-   # iterate through columns of Y
+   
    for j in range(len(Y[0])):
-       # iterate through rows of Y
+       
        for k in range(len(Y)):
            result[i][j] += X[i][k] * Y[k][j]
 
