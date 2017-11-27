687782e7b974652fd1291fbff828dd8b5294560b
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2017-11-27 23:04:40 +0530

    program to print pascal triangle

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -1,6 +1,7 @@
 n=int(input("Enter number of rows: "))
 a=[]
 for i in range(n):
+
     a.append([])
     a[i].append(1)
     for j in range(1,i):
@@ -8,7 +9,9 @@ for i in range(n):
     if(n!=0):
         a[i].append(1)
 for i in range(n):
+
     print("   "*(n-i),end=" ",sep=" ")
     for j in range(0,i+1):
+        
         print('{0:6}'.format(a[i][j]),end=" ",sep=" ")
     print()
