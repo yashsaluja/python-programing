47731f803c1c017fa52a09cde757646f24737493
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-01-24 23:07:08 +0530

    program for insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -5,11 +5,14 @@ def insertionSort(alist):
      position = index
 
      while position>0 and alist[position-1]>currentvalue:
+         
          alist[position]=alist[position-1]
          position = position-1
 
      alist[position]=currentvalue
 
 alist = [54,26,93,17,77,31,44,55,20]
+
 insertionSort(alist)
+
 print(alist)
