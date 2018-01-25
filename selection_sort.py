0fb36bfdbdb06d3b83aa767c425c21df91e33b20
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-01-25 22:13:46 +0530

    program for selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,6 +1,8 @@
 def selectionSort(alist):
    for fillslot in range(len(alist)-1,0,-1):
+
        positionOfMax=0
+
        for location in range(1,fillslot+1):
 
            if alist[location]>alist[positionOfMax]:
@@ -14,5 +16,7 @@ def selectionSort(alist):
        alist[positionOfMax] = temp
 
 alist = [54,26,93,17,77,31,44,55,20]
+
 selectionSort(alist)
+
 print(alist)
