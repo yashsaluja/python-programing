934818fd68222433852dbe379e8e9c3f8834577a
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-01-26 21:22:33 +0530

    program toprint pascal triangle

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -4,11 +4,14 @@ a=[]
 for i in range(n):
 
     a.append([])
+
     a[i].append(1)
 
     for j in range(1,i):
+
         a[i].append(a[i-1][j-1]+a[i-1][j])
     if(n!=0):
+        
         a[i].append(1)
         
 for i in range(n):
