0d252d10f73f3ce5204d805fee8383fe878e052c
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-01-27 11:25:07 +0530

    program to find a leap year

--- leap_year.py
+++ leap_year.py
@@ -3,10 +3,8 @@ year = 2000
 year = int(input("Enter a year: "))
 
 if (year % 4) == 0:
-
-   if (year % 100) == 0:
-
-       if (year % 400) == 0:
+    if (year % 100) == 0:
+    if (year % 400) == 0:
            
            print("{0} is a leap year".format(year))
        else:
