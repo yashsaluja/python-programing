1cfbed4b267b2cf53c8c723cb6d2e8dfa02dcdc6
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-01-27 11:28:01 +0530

    program to find largest of three numbers

--- largest.py
+++ largest.py
@@ -5,7 +5,9 @@ num3 = 12
 
  
 num1 = float(input("Enter first number: "))
+
 num2 = float(input("Enter second number: "))
+
 num3 = float(input("Enter third number: "))
 
 if (num1 >= num2) and (num1 >= num3):
